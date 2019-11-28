c2f87b864451a9a2c5715c38b6865879c7aca909
Ninngi <thepatrvq@gmail.com>
2019-11-28 21:39:55 +0100

    Udagram Init

---
+++ src/config/config.ts
@@ -0,0 +1,19 @@
+export const config = {
+  "dev": {
+    "username": process.env.POSTGRESS_username,
+    "password": process.env.POSTGRESS_password,
+    "database": process.env.POSTGRESS_database,
+    "host": process.env.POSTGRESS_host || "upatrvqdb.cwyfsqhp4ite.eu-central-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": process.env.AWS_region,
+    "aws_profile": process.env.AWS_profilem,
+    "aws_media_bucket": process.env.AWS_media_bucket
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
