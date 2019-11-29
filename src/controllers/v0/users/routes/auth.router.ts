17525ce2efc5b254cdad4dcc6775c1380aaa6b2a
Ninngi <thepatrvq@gmail.com>
2019-11-29 03:24:18 +0100

    implementing auth

--- src/controllers/v0/users/routes/auth.router.ts
+++ src/controllers/v0/users/routes/auth.router.ts
@@ -9,7 +9,7 @@ import { NextFunction } from 'connect';
 import * as EmailValidator from 'email-validator';
 import { config } from '../../../../config/config';
 
-import { config } from 'bluebird'
+// import { config } from 'bluebird'
 
 const router: Router = Router();
 
@@ -32,25 +32,25 @@ function generateJWT(user: User): string {
 }
 
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
-    return next();
-    // if (!req.headers || !req.headers.authorization){
-    //     return res.status(401).send({ message: 'No authorization headers.' });
-    // }
+    // return next();
+    if (!req.headers || !req.headers.authorization) {
+        return res.status(401).send({ message: 'No authorization headers.' });
+    }
 
+    // Bearer asdjfhajskdfhklashfdklashdfkl  
+    const token_bearer = req.headers.authorization.split(' ');
+    if (token_bearer.length != 2) {
+        return res.status(401).send({ message: 'Malformed token.' });
+    }
 
-    // const token_bearer = req.headers.authorization.split(' ');
-    // if(token_bearer.length != 2){
-    //     return res.status(401).send({ message: 'Malformed token.' });
-    // }
+    const token = token_bearer[1];
 
-    // const token = token_bearer[1];
-
-    // return jwt.verify(token, "hello", (err, decoded) => {
-    //   if (err) {
-    //     return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
-    //   }
-    //   return next();
-    // });
+    return jwt.verify(token, config.jwt.secret, (err, decoded) => {
+        if (err) {
+            return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
+        }
+        return next();
+    });
 }
 
 router.get('/verification',
@@ -87,7 +87,6 @@ router.post('/login', async (req: Request, res: Response) => {
 
     // Generate JWT
     const jwt = generateJWT(user);
-
     res.status(200).send({ auth: true, token: jwt, user: user.short() });
 });
 
