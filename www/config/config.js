07db2814ee3aa24d0e28a3edcc5ac60ec2f98e7b
Ninngi <thepatrvq@gmail.com>
2019-11-29 03:29:18 +0100

    .gitignore

---
+++ www/config/config.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = {
+    "dev": {
+        "username": process.env.POSTGRESS_username,
+        "password": process.env.POSTGRESS_password,
+        "database": process.env.POSTGRESS_database,
+        "host": process.env.POSTGRESS_host,
+        "dialect": "postgres",
+        "aws_region": process.env.AWS_region,
+        "aws_profile": process.env.AWS_profilem,
+        "aws_media_bucket": process.env.AWS_media_bucket
+    },
+    "prod": {
+        "username": "",
+        "password": "",
+        "database": "udagram_prod",
+        "host": "",
+        "dialect": "postgres"
+    },
+    "jwt": {
+        "secret": "helloworld"
+    }
+};
+//# sourceMappingURL=config.js.map
